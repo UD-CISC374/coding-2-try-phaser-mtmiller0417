ee4237ac64c95e81e413c4b6a5f7cbe1b3b46d59
Matt <matt.miller041798@gmail.com>
2020-02-28 00:34:33 -0500

    remove music

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -15,21 +15,21 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('button','./../../assets/play-button-1.png');
+    this.load.image('play-button','./../../assets/play-button-1.png');
     this.load.audio('music','../../assets/background-music.ogg');
   }
 
   create() {
 
-    this.scene.start('GameScene');
+    //this.scene.start('GameScene');
 
-    if(!Sound.sound_playing){
+    /*if(!Sound.sound_playing){
       this.music = this.sound.add('music',{loop: true});
       this.music.play();
       Sound.sound_playing = true;
-    }
+    }*/
 
-    this.play_button = this.add.image(0,0,'button');
+    this.play_button = this.add.image(0,0,'play-button');
     // Center the play button
     this.play_button.x = this.cameras.main.centerX;
     this.play_button.y = this.cameras.main.centerY;
