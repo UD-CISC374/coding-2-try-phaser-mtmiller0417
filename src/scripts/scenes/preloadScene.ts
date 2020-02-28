dcdc32f1e037721fb64325402009ba6ee29677c9
Matt <matt.miller041798@gmail.com>
2020-02-28 01:57:43 -0500

    added src/

--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -16,7 +16,7 @@ export default class PreloadScene extends Phaser.Scene {
 
   preload() {
     //this.load.image('play-button','./../../assets/play-button-1.png');
-    this.load.image('play-button','assets/play-button-1.PNG');
+    this.load.image('play-button','src/assets/play-button-1.png');
     //this.load.audio('music','../../assets/background-music.ogg');
   }
 
